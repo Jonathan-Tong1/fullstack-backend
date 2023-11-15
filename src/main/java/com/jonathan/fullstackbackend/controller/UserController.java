b7e0293cdf86b37cdf30fd145f9f77d489986145
Jonathan Tong <jontong321@gmail.com>
2023-11-15 13:01:01 -0500

    Exception Handling Added

--- src/main/java/com/jonathan/fullstackbackend/controller/UserController.java
+++ src/main/java/com/jonathan/fullstackbackend/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.jonathan.fullstackbackend.controller;
 
+import com.jonathan.fullstackbackend.exception.UserNotFoundException;
 import com.jonathan.fullstackbackend.model.User;
 import com.jonathan.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -25,5 +24,11 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
     }
 }
---
+++ src/main/java/com/jonathan/fullstackbackend/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jonathan.fullstackbackend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("Could not find the user with id " + id);
+    }
+}
